576e7a83cfd8b5e37fad8ab46f95d1d6aad2ff5c
Akash kumar <109230614+i-m-akash@users.noreply.github.com>
2024-10-01 19:58:03 +0530

    Update paymentModel.js

--- model/paymentModel.js
+++ model/paymentModel.js
@@ -1,19 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const paymentSchema = new mongoose.Schema({
-  razorpay_order_id: {
+  order_id: {
     type: String,
     required: true,
   },
-  razorpay_payment_id: {
+  payment_id: {
     type: String,
     required: true,
   },
-  razorpay_signature: {
-    type: String,
+  payment_time: {
+    type: Date,
     required: true,
   },
-});
+  amount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["captured", "failed"], // This ensures the status can only be either "captured" or "failed"
+  },
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
 
-const Payment = mongoose.model("Payment", paymentSchema);
-module.exports =Payment;
+const Payment = mongoose.model('Payment', paymentSchema);
+
+module.exports = Payment;
